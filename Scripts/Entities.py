b6d6e62db1704086ac84a54880c1a2b43f8f4675
JZORs <jdsimancasf@gmail.com>
2026-02-11 19:20:17 -0400

    Add files via upload

---
+++ Scripts/Entities.py
@@ -0,0 +1,94 @@
+import pygame, random 
+ 
+class PhysicsEntity():
+    def __init__(self, game, e_type, position, size):
+        self.game = game
+        self.type = e_type
+        self.size = size
+        self.pos = list(position)
+        self.velocity = [0, 0]
+
+        self.action = ''
+        self.flip = False
+
+    def rect(self):
+        return pygame.Rect(self.pos[0], self.pos[1], self.size[0], self.size[1])
+    
+    def set_action(self, action):
+        if action != self.action:
+            self.action = action
+    
+    def update(self, tilemap, movement = (0,0)):
+        frame_movement = (movement[0] + self.velocity[0], movement[1] + self.velocity[1])
+
+        self.pos[0] += frame_movement[0]
+        entity_rect = self.rect()
+        for rect in tilemap.physics_rects_around(self.pos):
+            if entity_rect.colliderect(rect):
+                if frame_movement[0] > 0:
+                    entity_rect.right = rect.left
+                if frame_movement[0] < 0:
+                    entity_rect.left = rect.right
+                self.pos[0] = entity_rect.x
+
+        self.pos[1] += frame_movement[1]
+        entity_rect = self.rect()
+        for rect in tilemap.physics_rects_around(self.pos):
+            if entity_rect.colliderect(rect):
+                if frame_movement[1] > 0:
+                    entity_rect.bottom = rect.top
+                if frame_movement[1] < 0:
+                    entity_rect.top = rect.bottom
+                self.pos[1] = entity_rect.y
+    
+    def render(self, surf):
+        surf.blit(self.game.assets[self.type], self.pos)
+
+class Centipede(PhysicsEntity):
+    def __init__(self, game, position, size):
+        super().__init__(game, 'centipede', position, size)
+
+    def update(self, tilemap, movement = (0, 0)):
+        super().update(tilemap, movement)
+
+
+
+class Player(PhysicsEntity):
+    def __init__(self, game, position, size):
+        super().__init__(game, 'player', position, size)
+
+    def shoot(self):
+        self.game.projectiles.append([[self.rect().centerx, self.rect().centery], 5, 0])
+
+    def update(self, tilemap, movement = (0, 0)):
+        super().update(tilemap, movement)
+        frame_movement = (movement[0] + self.velocity[0], movement[1] + self.velocity[1])
+
+        entity_rect = self.rect()
+        if frame_movement[0] > 0:
+            self.velocity[0] = min(1.2, self.velocity[0] + 0.1)
+            if entity_rect.x + self.size[0] > self.game.display.get_width() - 14:
+                entity_rect.x = self.game.display.get_width() - 14 - self.size[0]
+                self.pos[0] = entity_rect.x
+        if frame_movement[0] < 0:
+            self.velocity[0] = min(-1.2, self.velocity[0] + 0.1)
+            if entity_rect.x < 14:
+                entity_rect.x = 14
+                self.pos[0] = entity_rect.x
+        entity_rect = self.rect()
+        if frame_movement[1] > 0:
+            if entity_rect.y + self.size[1] > self.game.display.get_height() - 14:
+                entity_rect.y = self.game.display.get_height() - 14 - self.size[1]
+                self.pos[1] = entity_rect.y
+        if frame_movement[1] < 0:
+            if entity_rect.y < self.game.display.get_height() - (96 + 14):
+                entity_rect.y = self.game.display.get_height() - (96 + 14)
+                self.pos[1] = entity_rect.y
+        
+        if movement[0] == 0:
+            self.velocity[0] = 0
+        if movement[1] == 0:
+            self.velocity[1] = 0
+
+    def render(self, surf):
+        super().render(surf)
---
+++ Scripts/Tilemap.py
@@ -0,0 +1,52 @@
+import pygame, random
+
+NEIGHBOR_OFFSETS = [(-1, 0), (-1, -1), (0, -1), (1, -1), (1, 0), (0, 0), (-1, 1), (0, 1), (1, 1)]
+
+class Tilemap():
+    def __init__(self, game, tile_size = 8):
+        self.game = game
+        self.tile_size = tile_size
+        self.random_pos = [list(range(2,60)), list(range(5, 40))]
+        self.tilemap = {}
+
+    def generate_map(self):
+        self.random_pos = [list(range(2,60)), list(range(5, 40))]
+        self.tilemap = {}
+        tiles_to_create = 50
+        while tiles_to_create > 0:
+            if not self.random_pos[0] or not self.random_pos[1]:
+                break
+                
+            x = random.choice(self.random_pos[0])
+            y = random.choice(self.random_pos[1])
+            
+            self.tilemap[str(x) + ';' + str(y)] = {'type': 'normal_m', 'variant': 0, 'pos': (x, y)}
+            
+            self.random_pos[0].remove(x)
+            self.random_pos[1].remove(y)
+            tiles_to_create -= 1
+
+    def tiles_around(self, pos):
+        tiles = []
+        tile_loc = (int(pos[0] // self.tile_size), int(pos[1] // self.tile_size))
+        for offset in NEIGHBOR_OFFSETS:
+            check_loc = str(tile_loc[0] + offset[0]) + ';' + str(tile_loc[1] + offset[1])
+            if check_loc in self.tilemap:
+                tiles.append(self.tilemap[check_loc])
+        return tiles
+    
+    def physics_rects_around(self, pos):
+        rects = []
+        for tile in self.tiles_around(pos):
+            rects.append(pygame.Rect(tile['pos'][0] * self.tile_size, tile['pos'][1] * self.tile_size, self.tile_size, self.tile_size))
+        return rects
+    
+    def solid_check(self, pos):
+        tile_loc = str(pos[0] // self.tile_size) + ';' + str(pos[1] // self.tile_size)
+        if tile_loc in self.tilemap:
+            return self.tilemap[tile_loc]
+            
+    def render(self, surf):
+        for loc in self.tilemap:
+            tile = self.tilemap[loc]
+            surf.blit(self.game.assets[tile['type']][tile['variant']], (tile['pos'][0] * self.tile_size, tile['pos'][1] * self.tile_size))
